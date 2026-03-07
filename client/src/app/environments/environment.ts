f7d3000e33f1e5923c4b444312cad16fa3dfde8d
Ruth <r0583215031@gmail.com>
2026-03-08 01:13:52 +0200

    Update API URL before render

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:5000'
+  // apiUrl: 'http://127.0.0.1:5000'
+    apiUrl: 'https://recipix-1rp5.onrender.com' // העליתי לרנדר בפרקטיקוד 4
+
 };
