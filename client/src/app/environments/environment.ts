24891161c326c8768b0bbe99154af418edbac7c1
Ruth <r0583215031@gmail.com>
2026-03-12 17:07:42 +0200

    Update API URL for AWS

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -1,6 +1,12 @@
-export const environment = {
-  production: false,
-  // apiUrl: 'http://127.0.0.1:5000'
-    apiUrl: 'https://recipix-1rp5.onrender.com' // העליתי לרנדר בפרקטיקוד 4
+// export const environment = {
+//   production: false,
+//   // apiUrl: 'http://127.0.0.1:5000'
+//     apiUrl: 'https://recipix-1rp5.onrender.com' // העליתי לרנדר בפרקטיקוד 4
 
+// };
+
+//לAWS כששיניתי 
+export const environment = {
+  production: true,
+  apiUrl: 'http://16.171.35.152:5000' // הכתובת של ה-Backend ב-AWS
 };
